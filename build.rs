57c9deabb89e94d210b69c46edd430a601345597
Klarhoefer, Udo <Udo.Klarhoefer@bakerhughes.com>
2020-11-25 08:42:40 +0100

    Search for link library in \Utils folder

--- build.rs
+++ build.rs
@@ -1,5 +1,26 @@
 
+use std::io;
+use std::fs;
+use std::path::Path;
+
+fn find_libzmq<P: AsRef<Path>>(path: P) -> io::Result<Option<String>> {
+    let entries = fs::read_dir(path)?;
+    for entry in entries {
+        if let Some(name) = entry?.file_name().to_str() {
+            if name.starts_with("libzmq") {
+                if let Some(pos) = name.rfind(".") {
+                    return Ok(Some((&name[..pos]).into()));
+                }
+            }
+        }
+    }
+    Ok(None)
+}
+
+
 fn main() {
-    println!("cargo:rustc-link-search=C:\\Utils");
-    println!("cargo:rustc-link-lib=libzmq-v120-mt-4_3_2");
+    let fldr = r"\Utils";
+    let name = find_libzmq(fldr).expect("Could not read directory").expect("Not found!");
+    println!("cargo:rustc-link-search={}", fldr);
+    println!("cargo:rustc-link-lib={}", name);
 }
